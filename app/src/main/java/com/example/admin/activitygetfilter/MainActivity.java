0a055c8702a0f340e0c7f9cb61cd0b3437c6ea49
DianaGloria <diana.maciel@cbtis72.edu.mx>
2019-04-25 14:08:02 -0500

    first comit

--- app/src/main/java/com/example/admin/activitygetfilter/MainActivity.java
+++ app/src/main/java/com/example/admin/activitygetfilter/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.admin.activitygetfilter;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+
+
+
+public class MainActivity extends AppCompatActivity implements ContactsAdapter.ContactsAdapterListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<Contact> contactList;
+    private ContactsAdapter mAdapter;
+    private SearchView searchView;
+
+    //url to fetch contacs json
+    private static final String URL = "http://puntosingular.mx/json/contacs.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +51,123 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // toolbar fancy stuff
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.toolbar_title);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        contactList = new ArrayList<>();
+        mAdapter = new ContactsAdapter(this, contactList, this);
+
+        //white background notificaton bar
+        whiteNotificationBar(recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, DividerItemDecoration.VERTICAL, 36));
+        recyclerView.setAdapter(mAdapter);
+
+        fetchContacs();
+
+    }
+
+    private void fetchContacs() {
+        JsonArrayRequest request = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response == null) {
+                    Toast.makeText(getApplicationContext(), "NO se pudo recuperar los contactos! Por favor intente de nuevo.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                List<Contact> items = new Gson().fromJson(response.toString(), new TypeToken<List<Contact>>() {
+                }.getType());
+                contactList.clear();
+                contactList.addAll(items);
+                mAdapter.notifyDataSetChanged();
+            },new Response.ErrorListener()
+
+            {
+                @Override
+                public void onErrorResponse (VolleyError error){
+                //error in getting json
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), "Erro: " + error.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+            }
+        });
+        MyApplication.getmInstance().addToRequestQueue(request);
+
+    }
+
+    @Override
+            public void onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Associate searchable configuration with the Searchview
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                // filter recycler view when query submitted
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void whiteNotificationBar(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int flags = view.getSystemUiVisibility();
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            view.setSystemUiVisibility(flags);
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
+    }
+    @Override
+    public void onContactSelected(Contact contact) {
+        Toast.makeText(getApplicationContext(), "Selected: " + contact.getName() + ", " + contact.getPhone(), Toast.LENGTH_LONG).show();
+
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +179,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,18 +187,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
